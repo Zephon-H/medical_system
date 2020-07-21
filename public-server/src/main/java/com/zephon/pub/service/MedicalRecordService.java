46a7eab6fd77b2c2e014087bfff4642a26b47d25
Zephon-H <1528492838@qq.com>
2020-07-21 16:00:41 +0800

    fix bug in outpatient-server and update jar

--- public-server/src/main/java/com/zephon/pub/service/MedicalRecordService.java
+++ public-server/src/main/java/com/zephon/pub/service/MedicalRecordService.java
@@ -1,8 +1,13 @@
 package com.zephon.pub.service;
 
+import com.zephon.common.model.Department;
+import com.zephon.common.model.Employee;
 import com.zephon.common.model.MedicalRecord;
 import com.zephon.common.service.BaseService;
+import com.zephon.pub.dao.DepartmentDao;
+import com.zephon.pub.dao.EmployeeDao;
 import com.zephon.pub.dao.MedicalRecordDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,4 +19,30 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MedicalRecordService extends BaseService<MedicalRecordDao, MedicalRecord> {
+    @Autowired
+    private EmployeeService employeeService;
+    @Autowired
+    private DepartmentService departmentService;
+    @Override
+    public MedicalRecord save(MedicalRecord medicalRecord) throws Exception {
+        try {
+            Employee doctor = employeeService.findById(medicalRecord.getDoctor().getId());
+            medicalRecord.setDoctor(doctor);
+        } catch (Exception e) {
+//            e.printStackTrace();
+        }
+        try {
+            Employee o = employeeService.findById(medicalRecord.getOperator().getId());
+            medicalRecord.setOperator(o);
+        } catch (Exception e) {
+//            e.printStackTrace();
+        }
+        try {
+            Department d = departmentService.findById(medicalRecord.getDepartment().getId());
+            medicalRecord.setDepartment(d);
+        } catch (Exception e) {
+//            e.printStackTrace();
+        }
+        return super.save(medicalRecord);
+    }
 }
